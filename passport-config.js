90a20de0cafd3c25642b05e961559a6414425b2c
JesPar30 <jesuzho@gmail.com>
2020-02-05 18:26:01 -0300

    act

---
+++ passport-config.js
@@ -0,0 +1,9 @@
+const passport = require('passport')
+
+
+
+
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
